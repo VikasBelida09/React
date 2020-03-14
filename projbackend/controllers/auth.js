f1b6b330f2ce293d69e7fa209c589edcbd18d40b
Juspay <juspay@Juspays-MacBook-Air.local>
2020-03-14 16:11:10 +0530

    full project is pushed

--- projbackend/controllers/auth.js
+++ projbackend/controllers/auth.js
@@ -1,18 +1,62 @@
 const User = require("../models/user")
+const {check,validationResult}=require('express-validator')
+const jwt=require('jsonwebtoken')
+const expressJwt=require('express-jwt')
+
 exports.signout=(req,res)=>{
    res.send("marega tu bsdk")
  
 }
 exports.signup=(req,res)=>{
+    console.log(req.body)
     const user=new User(req.body)
+    console.log(user)
+    const errors=validationResult(req)
+    if(!errors.isEmpty()) {
+        return res.status(422).json({
+            error:errors.array()[0].msg
+        })
+    }
     user.save((err,user)=>{
-        if(err){
-            return "madar chod error hai"
+        if(!err){
+            return res.send(err)
         }
+        return res.send(user)
+    })
+}
+exports.signin=(req,res)=>{
+    const {password,email}=req.body
+    const errors=validationResult(req)
+    if(!errors.isEmpty()) {
+        return res.status(422).json({
+            error:errors.array()[0].msg
+        })
+    }
+    User.findOne({email},(err,user)=>{
+        if(err || !user){
+            return res.status(400).json({
+                error:"user doesnt exists in our database"
+            })
+        }
+        if(!user.authenticate(password)){
+            return res.status(401).json({
+                error:"email and password doesnt match"
+            })
+        }
+        const token=jwt.sign({_id:user.id},process.env.SECRET)
+
+        res.cookie("token",token,{expire:new Date() + 9999})
+
+        const {email,_id,name,role}=user
         return res.json({
-            name:user.name,
-            id:user._id,
-            email:user.email
+            token,
+            user:{
+                _id,
+                name,
+                email,
+                role
+            }
         })
     })
 }
+
--- projbackend/routes/auth.js
+++ projbackend/routes/auth.js
@@ -1,6 +1,21 @@
 const express=require('express')
 const router=express.Router()
-const {signout,signup}=require("../controllers/auth")
+const User = require("../models/user")
+const {check,ValidationResult}=require('express-validator')
+const {signout,signup,signin}=require("../controllers/auth")
 router.get("/signout",signout)
-router.post("/signup",signup)
+router.post(
+    "/signup",
+    [check("name","name should of min length 3").isLength({min:3})
+]
+,signup)
+router.post(
+    "/signin",
+    [
+        //check("name","name should of min length 3").isLength({min:3}),
+        check("email","email is required and it should be in proper format").isEmail(),
+        check("password","password is required and it should be of minimum 6 letters").isLength({min:3})
+    ]
+,signin)
+
 module.exports=router

552debe75704885488298625acb64183d752604c
Vikas Belida <belida.vikas@juspay.in>
2020-04-29 18:00:26 +0530

    fixed navbar position to stay at the top

--- projfrontend/src/core/Menu.js
+++ projfrontend/src/core/Menu.js
@@ -12,7 +12,7 @@ const currentTab = (history, path) => {
 
 const Menu = ({ history }) => (
   <div>
-    <ul className="nav nav-tabs bg-dark">
+    <ul className="nav nav-tabs bg-dark fixed-top">
       <li className="nav-item">
         <Link style={currentTab(history, "/")} className="nav-link" to="/">
           Home
